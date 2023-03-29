432c05b32108ba89f9fcc8e2807bfbd381053c9e
Vlad Osmianski <vlaosm@gmail.com>
2023-03-29 23:23:07 +0300

    `#[PlainObject]` attribute renamed to `#[Hint]`

--- src/Hint.php
+++ src/Hint.php
@@ -6,7 +6,8 @@ use Attribute;
 
 /**
  * Marks a class as a hint that only serves for code completion
- * and that is never instantiated. Hint classes must be abstract.
+ * and that is never instantiated. Hint classes must be abstract and
+ * has `Hint` suffix.
  */
 #[Attribute(Attribute::TARGET_CLASS)]
 final class Hint
